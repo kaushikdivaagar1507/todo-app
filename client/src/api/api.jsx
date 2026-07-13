f97218be5f8df7785ee5efb2c6d5f7dae48134de
Kaushik <kaushikdivaagar@gmail.com>
2026-07-14 01:41:11 +0530

    Update API URL for production

--- client/src/api/api.jsx
+++ client/src/api/api.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://todo-app-963n.onrender.com/api/tasks",
+
 });
 
 export default API;
